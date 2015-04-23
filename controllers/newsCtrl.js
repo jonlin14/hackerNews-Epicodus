10f40e119011df7231c227236c5c55b23bb9026c
Jonathan Lin <student@epicodus.com>
2015-04-23 11:06:04 -0700

    Add reverse filtering

--- controllers/newsCtrl.js
+++ controllers/newsCtrl.js
@@ -1,14 +1,14 @@
 myApp.controller('newsCtrl', function newsCtrl($scope) {
-    $scope.stories = {
-        story : {
+    $scope.stories = [
+        {
             name :'Obama wins presidency',
             vote : 0
         },
-        story1 : {
+        {
             name :'Bp Oil Spill',
             vote : 0
         }
-    }
+    ]
     $scope.addValue = function(item) {
         item.vote += 1;
     }
