edde6b9ab35230303286d202d4e2e454917cc90d
Jonathan Lin <student@epicodus.com>
2015-04-23 10:23:19 -0700

    Add looping through a hardcoded set of data

---
+++ controllers/newsCtrl.js
@@ -0,0 +1,12 @@
+myApp.controller('newsCtrl', function newsCtrl($scope) {
+    $scope.stories = {
+        story : {
+            name :'Obama wins presidency',
+            value : 0
+        },
+        story1 : {
+            name :'Bp Oil Spill',
+            value : 0
+        }
+    }
+});
