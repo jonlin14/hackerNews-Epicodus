ca5fcb3e0c065536ff3481c785847d22af2b2db6
Jonathan Lin <student@epicodus.com>
2015-04-23 11:32:10 -0700

    Add NPR api to generate titles

--- controllers/newsCtrl.js
+++ controllers/newsCtrl.js
@@ -1,15 +1,22 @@
-myApp.controller('newsCtrl', function newsCtrl($scope) {
-    $scope.stories = [
-        {
-            name :'Obama wins presidency',
-            vote : 0
-        },
-        {
-            name :'Bp Oil Spill',
-            vote : 0
-        }
-    ]
+myApp.controller('newsCtrl', function newsCtrl($scope, $http) {
+
+
     $scope.addValue = function(item) {
         item.vote += 1;
     }
+
+    $http({
+        method: 'JSONP',
+        url: 'http://api.npr.org/query?id=1001,1003&fields=title&output=JSON&apiKey=MDE4OTMxNTM0MDE0Mjk2NzAwMzc1ODk3YQ001' + '&callback=JSON_CALLBACK'
+    }).success(function(data, status) {
+        $scope.programs = data.list.story
+        console.log($scope.programs)
+        for (i = 0; i < $scope.programs.length; i++) {
+            $scope.programs[i].vote = 0;
+        }
+
+    }).error(function(data,status) {
+        console.alert('ERROR')
+    });
+
 });
