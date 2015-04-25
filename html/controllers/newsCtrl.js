4283ba5aaf720dd667fd16dcdce935494cc25b35
Jonathan Lin <johnlin0214@gmail.com>
2015-04-24 21:19:16 -0400

    Change layout of folders

---
+++ html/controllers/newsCtrl.js
@@ -0,0 +1,25 @@
+myApp.controller('newsCtrl', function newsCtrl($scope, $http) {
+
+
+    $scope.addValue = function(item) {
+        item.vote += 1;
+    }
+    $scope.lowerValue = function(item) {
+        item.vote -= 1;
+    }
+
+    $http({
+        method: 'JSONP',
+        url: 'http://api.npr.org/query?id=1001,1003&output=JSON&apiKey=MDE4OTMxNTM0MDE0Mjk2NzAwMzc1ODk3YQ001' + '&callback=JSON_CALLBACK'
+    }).success(function(data, status) {
+        $scope.programs = data.list.story
+        console.log($scope.programs)
+        for (i = 0; i < $scope.programs.length; i++) {
+            $scope.programs[i].vote = 0;
+        }
+
+    }).error(function(data,status) {
+        console.alert('ERROR')
+    });
+
+});
